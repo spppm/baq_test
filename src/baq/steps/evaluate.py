d6cf38553e47940eec556649256d255f607ae048
chogerlate <siwaratlaopromger4444@gmail.com>
2025-05-14 03:17:19 +0700

    feat: add evaluation module for forecasting model performance metrics

---
+++ src/baq/steps/evaluate.py
@@ -0,0 +1,6 @@
+"""
+This module contains the code for evaluating the model.
+- In this case, we develop forecasting models, so the evaluation is done by comparing the predicted values with the actual values.
+- The evaluation metrics are the mean absolute error, the mean squared error, and the R-squared score.
+- The evaluation is done on the test set.
+"""
